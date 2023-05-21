77d378f3873b4474f40791636eda92135cf99529
Gene R <gururise@gmail.com>
2023-05-21 12:39:18 -0700

    updates

--- server.py
+++ server.py
@@ -1,3 +1,23 @@
+"""
+server.py
+Author: Gene Ruebsamen
+
+This file is part of OpenAI-Text-Generation-Inference-Server.
+
+OpenAI-Text-Generation-Inference-Server is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenAI-Text-Generation-Inference-Server is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenAI-Text-Generation-Inference-Server. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
